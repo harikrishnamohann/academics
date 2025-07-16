78c13046b920c55acc317795dbb18f0874d17ff8
Harii <harikrishnamohan@proton.me>
2025-07-16 23:58:16 +0530

    gitignore rule to ignore executables updated

--- system-software-lab/cycle1/exp3/fifo_page_replacement.c
+++ system-software-lab/cycle1/exp3/fifo_page_replacement.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// returns posigion if array has val; -1 otherwise
+// returns true if array has val; false otherwise
 bool contains(int arr[], int size, int val) {
   for (int i = 0; i < size; i++) {
     if (arr[i] == val) return true;
   }
   return false;
-}
-
-// circularly insert values to array.
-void insert(int arr[], int size, int* ptr, int val) {
-  *ptr = (*ptr + 1) % size;
-  arr[*ptr] = val;
 }
 
 int main() {
@@ -22,7 +16,7 @@ int main() {
   printf("Enter length of reference string: ");
   scanf("%d", &ref_size);
 
-  int ref[ref_size], frames[frame_size], current_frame = -1;
+  int ref[ref_size], frames[frame_size], current_frame = -1, hits = 0;
   for(int i = 0; i < frame_size; i++) frames[i] = -1;
 
   printf("Enter reference string\n");
@@ -30,7 +24,6 @@ int main() {
     scanf("%d", &ref[i]);
   }
 
-  int hits = 0;
   printf("req\tstatus     \tframes\n");
   for (int i = 0; i < ref_size; i++) {
     printf("%3d\t", ref[i]);
@@ -39,7 +32,8 @@ int main() {
       hits++;
     } else { // page fault
       printf("page-fault\t");
-      insert(frames, frame_size, &current_frame, ref[i]);
+      current_frame = (current_frame + 1) % frame_size; // circular array implementation
+      frames[current_frame] = ref[i];
     }
     for (int j = 0; j < frame_size; j++) {
       if (frames[j] < 0) printf("_, ");
@@ -47,7 +41,6 @@ int main() {
     }
     printf("\n");
   }
-
   printf("\nnumber of page hits = %d\n", hits);
   printf("number of page faults = %d\n", ref_size - hits);
 
