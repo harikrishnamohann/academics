773f431c109ec6e550b64c84565bb2bcf6e09e97
Harii <harikrishnamohan@proton.me>
2025-08-05 21:11:04 +0530

    pass1 algorithm completed

--- system-software-lab/cycle2/pass1.c
+++ system-software-lab/cycle2/pass1.c
@@ -14,94 +14,112 @@ Outputs:
 #include <string.h>
 
 #define SIC_FILE "./input.sic"
-#define OPTAB_FILE "./optab.txt"
-#define SYMTAB_FILE "./symtab.txt"
-#define PASS1_INTR_FILE "./pass1_intermediate_file.dat"
+#define OPTAB_FILE "./optab"
+#define SYMTAB_FILE "./symtab"
+#define PASS1_INTR_FILE "./pass1_intermediate_file"
 
-#define OPCODE_LENGTH 3
+#define INSTRUCTION_SIZE 3
 #define BUF_SIZE 20
 
 char opcode[BUF_SIZE], operand[BUF_SIZE], label[BUF_SIZE];
 
-bool contains(FILE* table, char* value) {
+// checks if table contains entity in key field in a key-value paired table
+// for optab and symtab
+bool contains(FILE* table, char* entity) {
+	rewind(table);
 	char key[BUF_SIZE], val[BUF_SIZE];
 	bool contains = false;
-	fscanf(table, "%s\t%s\n", key, val);
-	do {
-		if (strcmp(value, key) == 0) {
+	while (fscanf(table, "%s\t%s\n", key, val) != EOF) {
+		if (strcmp(entity, key) == 0) {
 			contains = true;
 			break;
 		}
-	} while (fscanf(table, "%s\t%s\n", key, val) != EOF);
-	rewind(table);
+	} 
 	return contains;
 }
 
+// saves current line of the sic program to corresponding fields
+// and advances file pointer
 void advance_line(FILE* program) {
-	memset(opcode, '\0', sizeof(char) * BUF_SIZE);
-	memset(operand, '\0', sizeof(char) * BUF_SIZE);
-	memset(label, '\0', sizeof(char) * BUF_SIZE);
-	fscanf(program, "%s\t%s\t%s\n", label, opcode, operand);
-	// printf("%s\t%s\t%s\n", label, opcode, operand);
+	int count = fscanf(program, "%s\t%s\t%s\n", label, opcode, operand);
+	if (count != 3) {
+		printf("error: malformated input found\n");
+		exit(1);
+	}
 }
 
-void write_to_pass1_file(FILE* output, int address) {
-	fprintf(output, "%4d\t%s\t%s\t%s\n", address, label, opcode, operand);
+void write_pass1(FILE* pass1_file, int address) {
+	fprintf(pass1_file, "%04X\t%s\t%s\t%s\n", address, label, opcode, operand);
 }
 
-void insert_label(FILE* symtab, char* label, int locctr) {
-	fprintf(symtab, "%s\t%s\n", label, locctr);
+// insert symbol to symtab
+void insert_symbol(FILE* symtab, char* label, int locctr) {
+	fprintf(symtab, "%s\t%04X\n", label, locctr);
+}
+
+int eval_byte_length(char* stream) {
+	int length = strlen(stream) - 3; // 3 for C'' or X'' characters
+	if (stream[0] == 'X') {
+		return length / 2;
+	}
+	return length;
 }
 
 int main() {
-	FILE* sic_program = fopen(SIC_FILE, "r");
-	FILE* optab = fopen(OPTAB_FILE, "r");
-	FILE* symtab = fopen(SYMTAB_FILE, "w");
-	FILE* pass1_output = fopen(PASS1_INTR_FILE, "w");
-	if (sic_program == NULL || optab == NULL || symtab == NULL || pass1_output == NULL) {
+	FILE* sic_file = fopen(SIC_FILE, "r");
+	FILE* optab_file = fopen(OPTAB_FILE, "r");
+	FILE* symtab_file = fopen(SYMTAB_FILE, "w+");
+	FILE* pass1_file = fopen(PASS1_INTR_FILE, "w");
+	if (sic_file == NULL || optab_file == NULL || symtab_file == NULL || pass1_file == NULL) {
 		perror("error opening files");
 	}
 	
 	int starting_addr = 0, locctr = 0;
-	advance_line(sic_program);
+	advance_line(sic_file);
 	if (strcmp(opcode, "START") == 0) {
 		starting_addr = atoi(operand);
 		locctr = starting_addr;
-		write_to_pass1_file(pass1_output, locctr);
+		write_pass1(pass1_file, locctr);
 	}
 	
-
-	while (strcmp(opcode, "END") == 0) {
-		advance_line(sic_program);
-		if (*label != '-') {
-			if (contains(symtab, label)) {
+	advance_line(sic_file);
+	while (strcmp(opcode, "END") != 0) {
+		if (label[0] != '-') {
+			if (contains(symtab_file, label)) {
 				printf("duplicate label found: %s at %d \n", label, locctr);
 				goto end_pass1;
 			} else {
-				insert_label(symtab, label, locctr);
+				insert_symbol(symtab_file, label, locctr);
 			}
 		}
 		
-		if (contains(optab, opcode)) {
-			if (strcmp(opcode, "WORD") == 0) {
-				locctr += OPCODE_LENGTH;
-			} else if (strcmp(opcode, "RESW") == 0) {
-				locctr += 3 * strlen(operand);
-				// todo
-			}
+		write_pass1(pass1_file, locctr);
+		if (contains(optab_file, opcode)) {
+			locctr += INSTRUCTION_SIZE;
 		} else {
-			printf("invalid operand found %s at %d\n", opcode, locctr);
-			goto end_pass1;
+ 			if (strcmp(opcode, "WORD") == 0) {
+				locctr += INSTRUCTION_SIZE;
+			} else if (strcmp(opcode, "BYTE") == 0) {
+				locctr += eval_byte_length(operand);
+			} else if (strcmp(opcode, "RESW") == 0) {
+				locctr += 3 * atoi(operand);
+			} else if (strcmp(opcode, "RESB") == 0) {
+				locctr += atoi(operand);
+			} else {
+				printf("invalid operand found %s at %d\n", opcode, locctr);
+				goto end_pass1;
+			}
 		}
-		
+		advance_line(sic_file);
 	}
 	
-	
+	write_pass1(pass1_file, locctr);
+	printf("program length: %04X\n", locctr - starting_addr);
 	
 	end_pass1:
-		fclose(sic_program);
-		fclose(optab);
-		fclose(symtab);
-		fclose(pass1_output);
+		fclose(sic_file);
+		fclose(optab_file);
+		fclose(symtab_file);
+		fclose(pass1_file);
 		return 0;
 }
