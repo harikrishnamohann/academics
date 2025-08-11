a68941a82e2268f52d15bb67e2d8c10f2ca47bcf
Harii <harikrishnamohan@proton.me>
2025-08-11 22:31:51 +0530

    shh

--- .stash/whisper.c
+++ .stash/whisper.c
@@ -1,39 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <assert.h>
 
-#define VAULT_FILE "./.secrets"
-#define BUF_SIZE 256
+#define VAULT_FILE "./.secrets1"
+#define BUF_SIZE 512
+
+typedef struct {
+    char* str;
+    size_t len;
+} string;
+
+string str_init(size_t size) {
+    return (string) {
+        .len = size,
+        .str = malloc(sizeof(char) * size)
+    };
+}
+
+void str_free(string* str) {
+    str->len = 0;
+    free(str->str);
+    str->str = NULL;
+}
 
 void usage(const char *prog_name) {
     printf("Usage: %s [--encode | --decode]\n", prog_name);
 }
 
-void decode(int key) {
-    FILE *fp = fopen(VAULT_FILE, "r");
-    if (fp == NULL) {
+void decode(string key) {
+    FILE *vault = fopen(VAULT_FILE, "r");
+    if (vault == NULL) {
         printf("vault not found\n");
         return;
     }
 
-    char buf[BUF_SIZE];
-    size_t bytes_read = fread(buf, sizeof(char), sizeof(buf), fp);
+    string secret = { key.str + (key.len / 2), key.len / 2 };
+    size_t bytes_read = fread(secret.str, sizeof(char), secret.len, vault);
     for (int i = 0; i < bytes_read; i++) {
-        buf[i] ^= key;
+        putchar(secret.str[i] ^ key.str[i % bytes_read]);
     }
-    printf("%s\n", buf);
-    fclose(fp);
+    putchar('\n');
+    fclose(vault);
 }
 
-void encode(char* secret, int size, int key) {
+void encode(string secret, string key) {
     FILE *fp = fopen(VAULT_FILE, "w");
     assert(fp != NULL);
 
-    for (int i = 0; i < size; i++) {
-        secret[i] ^= key;
+    for (int i = 0; i < secret.len; i++) {
+        secret.str[i] ^= key.str[i % secret.len]; 
     }
-    fwrite(secret, sizeof(char), size, fp);
+    fwrite(secret.str, sizeof(char), secret.len, fp);
     fclose(fp);
 }
 
@@ -43,22 +62,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int key;
+    string buf = str_init(sizeof(char) * BUF_SIZE);
     if (strcmp(argv[1], "--encode") == 0) {
       printf("what's your secret message?\n > ");
-      char buf[BUF_SIZE];
-      fgets(buf, sizeof(buf), stdin);
-      printf("authentication key please\n > ");
-      scanf("%d", &key);
-      encode(buf, strlen(buf) - 1, key);
+      fgets(buf.str, buf.len / 2, stdin);
+
+      printf("Enter pass phrase\n > ");
+      fgets(buf.str + (buf.len / 2), buf.len / 2, stdin);
+
+      string secret = { buf.str, strlen(buf.str) - 1 };
+      string key = { buf.str + (buf.len / 2), strlen(buf.str + (buf.len / 2)) - 1 };
+      encode(secret, key);
     } else if (strcmp(argv[1], "--decode") == 0) {
-      printf("Feeling hungry?\n > ");
-      scanf("%d", &key);
-      decode(key);
+      printf("pass phrase please...\n > ");
+      fgets(buf.str, buf.len, stdin);
+      decode(buf);
     } else {
         usage(argv[0]);
         return 1;
     }
 
+    str_free(&buf);
     return 0;
 }
